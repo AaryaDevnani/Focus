17ca0e5ad1d92257f2fd9289704b3f1a553b3861
Aarya Devnani <aaryadevnani@Aaryas-MacBook-Pro.local>
2021-03-27 02:45:48 +0530

    removed console logs and added confirm message

--- extension/background/background.js
+++ extension/background/background.js
@@ -8,16 +8,20 @@ let restrictedSitesVisited = [];
 
 chrome.tabs.onActivated.addListener(tab => {
     chrome.tabs.get(tab.tabId, current_tab_info =>{
-        //console.log(current_tab_info.url)
-        let url = current_tab_info.url
         current_url = current_tab_info.url.split("/")[2]
         let a = restricted_sites.indexOf(current_url);
+        console.log(tab.title)
         //console.log(a)
         if(current_url == "newtab" || current_url == undefined){
-            console.log("kal")
+            //console.log("kal")
         }
         else if(a >-1){
-            alert("scam mat kar nahi toh mummy ko bolega")
+            result = confirm("This is a warning, you are visiting a restricted website. Click 'OK' to go back, or else a notification will be sent to your parent/guardian")
+            if(result == true){
+                chrome.tabs.remove(tab.tabId)
+            }else{
+                //execute email code
+            }
             restrictedSitesVisited.push(current_url)
             history.push(current_url)
             console.log(history)
@@ -26,11 +30,7 @@ chrome.tabs.onActivated.addListener(tab => {
         else{
             history.push(current_url)
             console.log(history)
-
-            //console.log("this")
-            //alert("scam mat kar nahi toh mummy ko bolega")
         }
     }
 )
 });
-
