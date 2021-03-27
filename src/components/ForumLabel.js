8791177dde7efd1e6216fb860ea2c22bc8723848
Kavish Shah <kavishshah09@gmail.com>
2021-03-28 04:23:34 +0530

    navlink for forum

--- src/components/ForumLabel.js
+++ src/components/ForumLabel.js
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import { useSelector } from 'react-redux'
 import { useHistory, useLocation } from 'react-router'
-import ForumSideNav from './ForumSideNav'
 import ForumMessages from './ForumMessages'
+import '../forum.css'
 
 function ForumLabel({match}) {
 
@@ -12,9 +12,9 @@ function ForumLabel({match}) {
     let { from } = location.state || { from: { pathname: "/login" } };
 
     const auth = useSelector(state => state.auth)
-    // if (!auth.loggedIn) {
-    //     history.replace(from)
-    // }
+    if (!auth.loggedIn) {
+        history.replace(from)
+    }
     
     const [messages, setMessages] = useState([])
 
@@ -31,14 +31,79 @@ function ForumLabel({match}) {
 		}
 	}
 
+    const [userInput, setUserInput] = useState({
+		message: '',
+		label: 'general',
+        user_id: auth.user.id,
+        username: auth.user.username
+	})
+
+    const handleOnChange = (e) => {
+		setUserInput({ ...userInput, [e.target.name]: e.target.value })
+	}
+    
+    const handleOnSubmit = (e) => {
+		e.preventDefault()
+		addMessage()
+	}
+
+    const messageOptions = {
+		method: 'POST',
+		body: JSON.stringify(userInput),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	}
+	const addMessage = async () => {
+		const response = await fetch(`/api/forum/${userInput.label}`, messageOptions)
+		const data = await response.json()
+		if (data.error_message === "") {
+			setUserInput({
+				message: '',
+                label: 'general',
+                user_id: auth.user.id,
+                username: auth.user.username
+			})
+            getMessages()
+		} else {
+			alert(data.error_message)
+		}
+	}
+
     useEffect(() => {
         getMessages()
     }, [match.params.label])
     
     return (
         <div>
-            <ForumSideNav />
-            <ForumMessages messages={messages} />
+            <div className="container bootstrap snippets bootdey">
+                <div className="row">
+                    <div className="col-md-12 bg-white ">
+                        <div className="chat-message">
+                            <form onSubmit={handleOnSubmit}>
+                                <label className='forumLabel'>Message  </label>
+                                <label className='forumLabel2'>Label  </label>
+                                <div className="input-group">
+                                    <input className="form-control border no-shadow no-rounded" placeholder="Type your message here" name="message" value={userInput.message} onChange={handleOnChange} />
+                                    <div>        
+                                        <select id="label" className='selectTag center' name="label" onChange={handleOnChange}>
+                                            <option className="center" value="general" selected>General</option>
+                                            <option className="center" value="browsing_history">Browsing History</option>
+                                            <option className="center" value="social_media">Social Media</option>
+                                        </select>
+                                    </div>
+                                    <span className="input-group-btn">
+                                        <button className="btn btn-success no-rounded" type="button">Send</button>
+                                    </span>
+                                </div>
+                            </form>
+                            <ul className="chat">
+                                <ForumMessages messages={messages} />
+                            </ul>
+                        </div>
+                    </div>        
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/ForumMessages.js
+++ src/components/ForumMessages.js
@@ -1,12 +1,41 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function ForumMessages({messages}) {
+
+    const auth = useSelector(state => state.auth)
+    
     return (
         <div>
             {messages.map((message) => (
-                <React.Fragment key={message.id}>
-                    <p className="center">{message.message}</p>
-                    <p className="center"> By {message.username}</p>
+                <React.Fragment>
+                    {
+                        message.user_id == auth.user.id 
+                        ?
+                            <li className="left clearfix">
+                                <div className="chat-body clearfix">
+                                    <div className="header">
+                                        <strong className="primary-font">{message.username}</strong>
+                                        <small class="pull-right text-muted">{message.label}</small>
+                                    </div>
+                                    <p>
+                                        {message.message}
+                                    </p>
+                                </div>
+                            </li>
+                        : 
+                        <li className="right clearfix">
+                                <div className="chat-body clearfix">
+                                    <div className="header">
+                                        <strong className="primary-font">{message.username}</strong>
+                                        <small class="pull-right text-muted">{message.label}</small>
+                                    </div>
+                                    <p>
+                                        {message.message}
+                                    </p>
+                                </div>
+                            </li>
+                    }
                 </React.Fragment>
             ))}
         </div>
