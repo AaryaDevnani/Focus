0f97517b1731cca6be68994c9df55799d4b29f3d
Kavish Shah <kavishshah09@gmail.com>
2021-03-28 09:10:35 +0530

    amogh changed home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ImageSlider from './ImageSlider'
+import img1 from '../Images/ImageO1.jpeg'
+import {Image} from 'react-bootstrap'
 
 function Home() {
     return (
-        <div>
+        <div className='rem-bg'>
             <ImageSlider/>
+            <Image src={img1} className='howTo-img' />
         </div>
     )
 }
